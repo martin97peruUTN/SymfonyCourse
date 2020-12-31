0c4ae39375b87294fc88c90349b93f6494ce898c
Martin Perussini <mperussini@frsf.utn.edu.ar>
2020-12-30 21:55:08 -0300

    2:08:00

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,7 +3,32 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController{
+class MainController extends AbstractController
+{
+    /** antes en el / decia /main, con el / redirige la pagina principal para aca
+     * @Route("/", name="casita")
+     */
+    public function index(): Response
+    {
+        return $this->render("home/index.html.twig");
+    }
 
+    /** el ? lo hace opcional
+     * @Route("/custom/{name?}", name="customPage")
+     * @param Request $req
+     * @return Response
+     */
+
+    public function custom(Request $req) {
+        //$name=dump($req->get("name"));
+        $name=$req->get("name");
+
+        return $this->render("home/custom.html.twig", [
+            "nombrecito"=>$name,
+        ]);
+    }
 }
